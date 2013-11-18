058ce14a9af875039c9460c6aa9f9f28b2cb4475
Fred McDavid <fred@landmetrics.com>
2013-11-18 09:54:35 -0500

    trying to move the filter to a new location

--- config/initializers/fredcom_liquid_extensions.rb
+++ config/initializers/fredcom_liquid_extensions.rb
@@ -1,4 +1,4 @@
-require 'photo_filter'
+require 'liquid/photo_filter'
 Locomotive.configure do |config|
   Rails.logger.info "registering photo filter filter"
   Liquid::Template.register_filter( PhotoFilter )
